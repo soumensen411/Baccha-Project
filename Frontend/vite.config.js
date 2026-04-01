f405a5951c4cae06dcbae61134edc205b28b32ca
soumen_sen <144879914+soumensen411@users.noreply.github.com>
2026-04-01 18:16:47 +0000

    modified vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,10 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-  base: '/baccha-project/',
-})
-=======
   base: '/Baccha-Project/',
 })
->>>>>>> 265915cc2b093374cded2dc155139ad1c56b8017
