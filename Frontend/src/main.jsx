82f43c2f6e41bb81d3e4df3e55d453abf5c9d198
soumen_sen <144879914+soumensen411@users.noreply.github.com>
2026-04-01 23:51:09 +0600

    Fix GitHub Pages base path casing

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter }from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/baccha-project/">
+    <BrowserRouter basename="/Baccha-Project/">
       <App />
     </BrowserRouter>
   </StrictMode>,
